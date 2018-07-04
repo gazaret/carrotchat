acb270b68d7dca25e4ec916f2fb1f1e6d7774ea8
Nikolay Polukhin <polu-hin@mail.ru>
2018-07-04 17:34:09 +0400

    save message in db

--- server/chat/consumers.py
+++ server/chat/consumers.py
@@ -1,17 +1,54 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from accounts.models import Account
+from .models import Messages
 
 class ChatConsumer(WebsocketConsumer):
   def connect(self):
+    # TODO: сделать различные группы
+    self.room_name = 'general'
+    self.room_group_name = 'chat_' + self.room_name
+
+    async_to_sync(self.channel_layer.group_add)(
+      self.room_group_name,
+      self.channel_name
+    )
+
     self.accept()
 
   def disconnect(self, close_code):
-    pass
+    async_to_sync(self.channel_layer.group_discard)(
+      self.room_group_name,
+      self.channel_name
+    )
 
+  # Обработчик сообщений
   def receive(self, text_data):
     text_json = json.loads(text_data)
     message = text_json['message']
+    username = text_json['username']
+
+    account = Account(username=username)
+    model = Messages(account=account, message=message)
+    model.save()
+
+    # Отправляем сообщение в группу
+    async_to_sync(self.channel_layer.group_send)(
+      self.room_group_name,
+      {
+        'type': 'chat_message',
+        'message': message,
+        'username': username
+      }
+    )
+
+  # Слушатель события chat_message
+  def chat_message(self, event):
+    message = event['message']
+    username = event['username']
 
     self.send(text_data=json.dumps({
-      'message': message
+      'message': message,
+      'username': username
     }))
--- server/chat/models.py
+++ server/chat/models.py
@@ -2,10 +2,7 @@ from django.db import models
 from accounts.models import Account
 
 class Messages(models.Model):
-  account = models.ForeignKey(Account, on_delete=models.CASCADE)
+  account = models.ForeignKey(Account, default=None, on_delete=models.CASCADE)
   message = models.TextField(max_length=3000)
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
-
-  def __str__(self):
-    return self.message
